e3755995f35da85957ee1a9b8a41e52ed72b7f93
Chris <cselig@hotmail.com>
2020-06-15 09:48:33 -0600

    Add files via upload
    cleaned code and added comments

--- 2020_06_13/01_script/2020_06_13.R
+++ 2020_06_13/01_script/2020_06_13.R
@@ -2,19 +2,16 @@ library(tidyverse)
 library(ggtext)
 
 # Get the Data
-
 firsts <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-09/firsts.csv')
 science <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-09/science.csv')
 
+# Pick a color palette for category variable
 colors_palette <- c('#FFBC41','#067891','#A9AAB0','#EAEAEA','#BBACA3','#F2D6AE','#7C7D3D','#DF811D')
-# Get random list of firsts to use for labels
-#set.seed(1963823)
 
+# Choose which firsts I want to add labels for
 selected_rows <- c(1,10,22,31,69,97,113,143,149,232,260,365,399,428,479)
 # Create a position for labels
 position_number <- c(75,-50,15,-80,50,-20,95,15,-55,50,-15,-91,13,-45,68)
-# subset_rows <- firsts %>% 
-#     sample_n(size = 5, replace = TRUE)
 
 # Subset rows 
 subset_rows <- firsts[selected_rows,]
@@ -26,8 +23,6 @@ subset_rows <- subset_rows %>%
            name = str_to_title(name)
            ) %>% 
     mutate(label = str_glue('{year}: {name}- \n{accomplishment}')) %>%
-    
-    # mutate(position = sample(-100:100, 5, replace=FALSE)) %>% 
     mutate(position = position_number)
     select(year,label, category,position)
 
@@ -47,6 +42,7 @@ firsts_plot <- firsts %>%
     geom_jitter() +
     # add horizontal line for x-axis
     geom_hline(yintercept = 0, color = 'black') +
+    # Add the text box for labels
     geom_textbox(data = subset_rows, aes(label = label, x =  year,y = position),
                  size = 3,
                  vjust = ifelse(subset_rows$position > 0,0,1),
@@ -54,6 +50,7 @@ firsts_plot <- firsts %>%
                  label.color = NA,
               width = grid::unit(0.15, "npc"),
                  show.legend = FALSE) +
+    # Change the color of the category variable
     scale_color_manual(values = colors_palette) +
     # Custom theme
     theme_void() +
@@ -62,6 +59,7 @@ firsts_plot <- firsts %>%
          subtitle = 'A very small subset of interesting firsts \n #BlackLivesMatter',
          caption = 'African American Achievements | TidyTuesday | Chris Selig'
          )+
+    # Set limits for the plot to ensure all labels show
     ylim(-115,115)+
     xlim(1720,2030) +
     # Adjust theme elements
@@ -71,8 +69,6 @@ firsts_plot <- firsts %>%
         legend.text = element_text(colour="#CBF8DF"),
         plot.margin=unit(c(1,0.1,1.5,1.2),"cm"),
         panel.background = element_rect(fill = "black", colour = "black"),
-        # panel.grid.major = element_blank(),
-        # panel.grid.minor = element_blank(),
         plot.background = element_rect(fill = "black"),
         plot.title = element_text(
             size = 14,
